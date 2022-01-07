0213bb4af90e4cf8b542607a64162366beffe97b
natanaelgomes <End071400>
2022-01-07 10:57:27 -0300

    Adicionando projeto

--- Exercicios/ex010.py
+++ Exercicios/ex010.py
@@ -1,5 +1,7 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quanto em dólar pode comprar ($3.27)
 
-s = int (input('Digite o valor do seu salário'))
-#teste
+s = int (input('Digite o valor da sua carteira'))
+d = s / 3.27
+print('Você pode comprar {:.02f} em dolares' .format(d))
+
 
