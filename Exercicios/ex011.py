c566578af0eb970903a45ecb7555445337c9b5b8
natanaelgomes <End071400>
2022-01-07 11:21:57 -0300

    Adicionando projeto

--- Exercicios/ex011.py
+++ Exercicios/ex011.py
@@ -1,1 +1,9 @@
 #Faça um programa que leia LARGURA E ALTURA, calcule sua area e a quantidade de tinta para pintá-la, cada litro de tinta pinta área de 2m²
+
+a = int (input('Insira a altura da área:'))
+l = int (input('Insira a largura da área:'))
+
+al = a * l
+t = al/2
+
+print ('Será necessário {} latas de tinta para pintar {:.02f}m²'.format(t,al))
