7cb7240f6b3a7df7f515ee24ca6a58cb45d84c6a
natanaelgomes <End071400>
2022-01-07 11:05:13 -0300

    Adicionando projeto

--- Exercicios/ex009.py
+++ Exercicios/ex009.py
@@ -1,6 +1,16 @@
 # Faça um programa que leia um numero inteiro e mostre na tela sua tabuada
 
-n1 = int (input ('Digite um numero:'))
+n1 = int (input ('Digite um numero: '))
 
 print('Você inseriu o numero:',n1)
-print('Tabuada: ' ,n1)
+print('Tabuada de:' ,n1)
+print ('{} x 1: {}' .format(n1,n1*1))
+print ('{} x 2: {}' .format(n1,n1*2))
+print ('{} x 3: {}' .format(n1,n1*3))
+print ('{} x 4: {}' .format(n1,n1*4))
+print ('{} x 5: {}' .format(n1,n1*5))
+print ('{} x 6: {}' .format(n1,n1*6))
+print ('{} x 7: {}' .format(n1,n1*7))
+print ('{} x 8: {}' .format(n1,n1*8))
+print ('{} x 9: {}' .format(n1,n1*9))
+print ('{} x 10: {}' .format(n1,n1*10))
