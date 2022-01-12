c662f4f1537063aff0eed41527a72f7714ba4e16
natanaelgomes <End071400>
2022-01-12 10:53:09 -0300

    Adicionando projeto

--- Exercicios/ex016.py
+++ Exercicios/ex016.py
@@ -1,1 +1,6 @@
 #Crie um programa que leia um número real e mostre o número inteiro EX.: 6.127 > 6
+
+import math
+n = float (input ('Digite um numero: '))
+i = math.trunc(n) #variavel i de inteiro, recebe math.trunc(variavel)
+print('O numero {} convertido para inteiro é {}' .format(n, i))
