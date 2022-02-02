a268f469b65f4a6af3a8662fb4fe708d62abda57
natanaelgomes <End071400>
2022-02-02 18:41:24 -0300

    Adicionando projeto

--- Exercicios/ex046.py
+++ Exercicios/ex046.py
@@ -1,1 +1,7 @@
 #Faça um programa que mostre na tela uma contagem regressiva de 10 segundos, com pausa de 1 segundo
+
+from time import sleep
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('BUUUUUUUUUM')
--- Exercicios/ex047.py
+++ Exercicios/ex047.py
@@ -1,1 +1,5 @@
 #Faça um programa que mostre na tela os números pares de 1 a 50
+print('Contagem de 0 a 50 somente de números pares')
+for c in range(0, 51, 2):
+    print(c)
+print('Fim!')
