f1cc6b2a44c0cee86938ba5884715c5fc92c6623
natanaelgomes <End071400>
2022-02-07 18:46:41 -0300

    Adicionando projeto

--- Exercicios/ex051.py
+++ Exercicios/ex051.py
@@ -1,1 +1,8 @@
 #Faça um programa que leia o primeiro termo e a razão, no final mostre os 10 primeiros termos dessa progressão
+
+primeiro = int(input('Insira o termo: '))
+razao = int(input('Insira a razão: ' ))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo, razao):
+    print('{}' .format(c))
+print('FIM')
--- Exercicios/ex052.py
+++ Exercicios/ex052.py
@@ -1,1 +1,2 @@
 #Faça um programa que leia um numero inteiro e diga se ele é primo ou não
+
