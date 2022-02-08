e7492f49305f58983ff7e67235983c40c417ae30
natanaelgomes <End071400>
2022-02-08 18:23:00 -0300

    Adicionando projeto

--- Exercicios/ex052.py
+++ Exercicios/ex052.py
@@ -1,2 +1,16 @@
 #Faça um programa que leia um numero inteiro e diga se ele é primo ou não
 
+num = int(input('Insira um número: '))
+total = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end=' ')
+        total += 1
+    else:
+        print('\033[31m', end=' ')
+    print('{}' .format(c), end=' ')
+print('\nO Numero {} foi dividido {} vezes' .format(num, total))
+if total == 2:
+    print('É um número primo')
+else:
+    print('Não é  um número primo')
