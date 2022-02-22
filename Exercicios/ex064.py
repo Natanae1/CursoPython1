15bb25fa2cb3624ecbb8eb04f74ee17ddfe2c153
natanaelgomes <End071400>
2022-02-22 17:50:56 -0300

    Adicionando projeto

--- Exercicios/ex064.py
+++ Exercicios/ex064.py
@@ -1,1 +1,13 @@
 #Crie um programa que leia varios numeros inteiros. O programa só vai parar quando digitar 999 e no final mostre a soma de todos
+num = 0
+cont = 0
+soma = 0
+num = int(input('Insira um numero ou digite 999 para finalizar: '))
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input('Insira um numero: '))
+if cont == 0:
+    print('Nenhum número digitado!')
+print('A soma dos {} números digitados é {}.' .format(cont, soma))
+print('Finalizando. . .')
--- Exercicios/ex065.py
+++ Exercicios/ex065.py
@@ -1,1 +1,25 @@
 #Crie um programa que leia vários numeros inteiros. No final mostre a media, maior e menor valor. O programa deve perguntar se quer continuar a digitar
+
+opcao = 'S'
+soma = 0
+media = 0
+quantos = 0
+maior = 0
+menor = 0
+while opcao in 'Ss':
+    num = int(input('Insira um número: '))
+    soma += num
+    quantos += 1
+    if quantos == 1:
+        maior = num
+        menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    opcao = str(input('Deseja continuar ? [S/N] ')).strip().upper()[0]
+media = soma / quantos
+print('Você digitou {} numeros e a média foi {}' .format(quantos, media))
+print('O Maior valor foi {} e menor foi o {}' .format(maior, menor))
+
